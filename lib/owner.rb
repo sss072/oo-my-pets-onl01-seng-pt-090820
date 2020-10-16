4eecba7a118371d8f92f31628d757c9bac4b8e09
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-10-16 00:49:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,22 @@
 class Owner
   attr_reader :name 
+  @@all = []
   def initialize(name)
     @name = name 
     self.species
+    @@all << self 
   end
   def species 
     @species = human 
+  end 
+  def self.all 
+    @@all 
+  end 
+  def self.count 
+    @@all.count 
+  end 
+  def self.reset_all 
+    @@all.clear 
+  end 
+  
 end
