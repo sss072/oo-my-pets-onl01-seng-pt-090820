f1f944f14760a6fb86ed9e03f5bd85614e019b07
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-10-16 00:55:36 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,4 +1,16 @@
 class Cat
-  def initialize(name, owner) 
+  attr_accessor :owner, :mood  
+  attr_reader :name 
+  @@all = []
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    @@all << self 
+    
+  end
+  def self.all 
+    @@all 
   end 
+  
 end
